2ded51c83a5118ca3aeb87c29bad44997fefb238
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-06-01 15:09:12 +1000

    Added log statement on init()
    Helps verify logging framework is behaving as expected.

--- src/main/java/au/edu/aaf/shibext/handler/ShibExtensionNamespaceHandler.java
+++ src/main/java/au/edu/aaf/shibext/handler/ShibExtensionNamespaceHandler.java
@@ -2,16 +2,20 @@ package au.edu.aaf.shibext.handler;
 
 import au.edu.aaf.shibext.sharedtoken.SharedTokenDataConnectorBeanDefinitionParser;
 import net.shibboleth.ext.spring.util.BaseSpringNamespaceHandler;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  */
 public class ShibExtensionNamespaceHandler extends BaseSpringNamespaceHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ShibExtensionNamespaceHandler.class);
     public static final java.lang.String NAMESPACE = "urn:mace:aaf.edu.au:shibboleth:2.0:resolver:dc";
 
     @Override
     public void init() {
+        LOG.debug("Registering SharedTokenDataConnectorBeanDefinitionParser...");
         registerBeanDefinitionParser(
                 SharedTokenDataConnectorBeanDefinitionParser.TYPE_NAME,
                 new SharedTokenDataConnectorBeanDefinitionParser());
