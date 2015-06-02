d4b3c1663d74cbc1683d21ed81dbe703f165772e
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-06-02 10:52:53 +1000

    Switched from cobertua to JaCoCo for coverage
    - A coverage report is generated and displayed on each gradle build, making it
      easier to keep an eye on coverage.
    
    - The build breaks if minimum coverage isn't met. This will be beneficial for
      PRs.
    
    - I have set minimum coverage to 85% for all metrics. This is to be reviewed.
    
    - JaCoCo can be integrated with coveralls which we may use in the future.

--- src/test/java/au/edu/aaf/shibext/handler/ShibExtensionNamespaceHandlerTest.java
+++ src/test/java/au/edu/aaf/shibext/handler/ShibExtensionNamespaceHandlerTest.java
@@ -1,11 +1,26 @@
 package au.edu.aaf.shibext.handler;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class ShibExtensionNamespaceHandlerTest {
+
+    @InjectMocks
+    private ShibExtensionNamespaceHandler shibExtensionNamespaceHandler;
 
     @Test
     public void testInit() {
+        shibExtensionNamespaceHandler.init();
+    }
+
+    public ShibExtensionNamespaceHandler getShibExtensionNamespaceHandler() {
+        return shibExtensionNamespaceHandler;
+    }
+
+    public void setShibExtensionNamespaceHandler(ShibExtensionNamespaceHandler shibExtensionNamespaceHandler) {
+        this.shibExtensionNamespaceHandler = shibExtensionNamespaceHandler;
     }
 }
