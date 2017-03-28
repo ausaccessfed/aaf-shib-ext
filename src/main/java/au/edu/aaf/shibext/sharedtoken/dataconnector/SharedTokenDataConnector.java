ce0c4d0eaec0db08ba5e5a50d74c566026721958
Terry Smith <t.smith@aaf.edu.au>
2017-03-28 01:33:28 +0000

    Modified exception messages to more accuratly reflect what occurred

--- src/main/java/au/edu/aaf/shibext/sharedtoken/dataconnector/SharedTokenDataConnector.java
+++ src/main/java/au/edu/aaf/shibext/sharedtoken/dataconnector/SharedTokenDataConnector.java
@@ -110,16 +110,16 @@ public class SharedTokenDataConnector extends AbstractDataConnector {
         LOG.debug("Getting sourceAttributeId '" + sourceAttributeId + "' from resolvedAttributes");
         Map<String, ResolvedAttributeDefinition> resolvedAttributes = workContext.getResolvedIdPAttributeDefinitions();
         if (resolvedAttributes.get(sourceAttributeId) == null) {
-            throwResolutionException("Value '" + sourceAttributeId + 
-                                     "' could not be resolved. Source Attribute ID is Null.");
+            throwResolutionException("Attribute '" + sourceAttributeId + 
+                                     "' didn't resolve a value.");
         }
 
         IdPAttribute resolvedAttribute = resolvedAttributes.get(sourceAttributeId).getResolvedAttribute();
         List<IdPAttributeValue<?>> values = resolvedAttribute.getValues();
 
         if (values == null || values.size() != 1) {
-            throwResolutionException("Value '" + sourceAttributeId + 
-                                     "' could not be resolved. Null or multiple values where returned.");
+            throwResolutionException("Attribute '" + sourceAttributeId + 
+                                     "' either didn't resolve any values or resloved multiple values. Only one resolved value is expected.");
         }
 
         Object resolvedSourceAttributeObject = values.get(0);
